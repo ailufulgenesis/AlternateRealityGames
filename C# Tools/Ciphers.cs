543d1a47c526d7aabee939cc98a78da852f85e20
anon <anon@anon.com>
2026-01-30 04:16:47 -0600

    added shorthand and worked out encoding for binary

--- C# Tools/Ciphers.cs
+++ C# Tools/Ciphers.cs
@@ -4,32 +4,32 @@ using System.Security.Cryptography.X509Certificates;
 
 class Dictionaries
 {
-    public static Dictionary<string, string> binaryToChar = new Dictionary<string, string>{
-            {"01000001", "A"},{"01000010", "B"},{"01000011", "C"},{"01000100", "D"},{"01000101", "E"},{"01000110", "F"},{"01000111", "G"},{"01001000", "H"},{"01001001", "I"},{"01001010", "J"},
-            {"01001011", "K"},{"01001100", "L"},{"01001101", "M"},{"01001110", "N"},{"01001111", "O"},{"01010000", "P"},{"01010001", "Q"},{"01010010", "R"},{"01010011", "S"},{"01010100", "T"},
-            {"01010101", "U"},{"01010110", "V"},{"01010111", "W"},{"01011000", "X"},{"01011001", "Y"},{"01011010", "Z"},
+    public static Dictionary<string, char> binaryToChar = new Dictionary<string, char>{
+            {"01000001", 'A'},{"01000010", 'B'},{"01000011", 'C'},{"01000100", 'D'},{"01000101", 'E'},{"01000110", 'F'},{"01000111", 'G'},{"01001000", 'H'},{"01001001", 'I'},{"01001010", 'J'},
+            {"01001011", 'K'},{"01001100", 'L'},{"01001101", 'M'},{"01001110", 'N'},{"01001111", 'O'},{"01010000", 'P'},{"01010001", 'Q'},{"01010010", 'R'},{"01010011", 'S'},{"01010100", 'T'},
+            {"01010101", 'U'},{"01010110", 'V'},{"01010111", 'W'},{"01011000", 'X'},{"01011001", 'Y'},{"01011010", 'Z'},
             
-            {"01100001", "a"},{"01100010", "b"},{"01100011", "c"},{"01100100", "d"},{"01100101", "e"},{"01100110", "f"},{"01100111", "g"},{"01101000", "h"},{"01101001", "i"},{"01101010", "j"},
-            {"01101011", "k"},{"01101100", "l"},{"01101101", "m"},{"01101110", "n"},{"01101111", "o"},{"01110000", "p"},{"01110001", "q"},{"01110010", "r"},{"01110011", "s"},{"01110100", "t"},
-            {"01110101", "u"},{"01110110", "v"},{"01110111", "w"},{"01111000", "x"},{"01111001", "y"},{"01111010", "z"},
+            {"01100001", 'a'},{"01100010", 'b'},{"01100011", 'c'},{"01100100", 'd'},{"01100101", 'e'},{"01100110", 'f'},{"01100111", 'g'},{"01101000", 'h'},{"01101001", 'i'},{"01101010", 'j'},
+            {"01101011", 'k'},{"01101100", 'l'},{"01101101", 'm'},{"01101110", 'n'},{"01101111", 'o'},{"01110000", 'p'},{"01110001", 'q'},{"01110010", 'r'},{"01110011", 's'},{"01110100", 't'},
+            {"01110101", 'u'},{"01110110", 'v'},{"01110111", 'w'},{"01111000", 'x'},{"01111001", 'y'},{"01111010", 'z'},
             
-            {"00110001", "1"},{"00100001", "!"},{"00110010", "2"},{"01000000", "@"},{"00110011", "3"},{"00100011", "#"},{"00110100", "4"},{"00100100", "$"},{"00110101", "5"},{"00100101", "%"},
-            {"00110110", "6"},{"01011110", "^"},{"00110111", "7"},{"00100110", "&"},{"00111000", "8"},{"00101010", "*"},{"00111001", "9"},{"00101000", "("},{"00110000", "0"},{"00101001", "}"},
+            {"00110001", '1'},{"00100001", '!'},{"00110010", '2'},{"01000000", '@'},{"00110011", '3'},{"00100011", '#'},{"00110100", '4'},{"00100100", '$'},{"00110101", '5'},{"00100101", '%'},
+            {"00110110", '6'},{"01011110", '^'},{"00110111", '7'},{"00100110", '&'},{"00111000", '8'},{"00101010", '*'},{"00111001", '9'},{"00101000", '('},{"00110000", '0'},{"00101001", '}'},
             
-            {"00100000", " "},{"01100000", "`"},{"01111110", "~"},{"00101101", "-"},{"01011111", "_"},{"00111101", "="},
-            {"00101011", "+"},{"01011011", "["},{"01111011", "{"},{"01011101", "]"},{"01111101", "}"},{"01011100", "\\"},
-            {"01111100", "|"},{"00111011", ";"},{"00111010", ":"},{"00100111", "\'"},{"00100010", "\""},{"00101100", ","},
-            {"00111100", "<"},{"00101110", "."},{"00111110", ">"},{"00101111", "/"},{"00111111", "?"},{"00001010","\n"}
+            {"00100000", ' '},{"01100000", '`'},{"01111110", '~'},{"00101101", '-'},{"01011111", '_'},{"00111101", '='},
+            {"00101011", '+'},{"01011011", '['},{"01111011", '{'},{"01011101", ']'},{"01111101", '}'},{"01011100", '\\'},
+            {"01111100", '|'},{"00111011", ';'},{"00111010", ':'},{"00100111", '\''},{"00100010", '\"'},{"00101100", ','},
+            {"00111100", '<'},{"00101110", '.'},{"00111110", '>'},{"00101111", '/'},{"00111111", '?'},{"00001010", '\n'}
     };
     public static void Binary(char textToBinary){
         if (textToBinary == 'y')
         {
-            foreach(KeyValuePair<string, string> kvp in binaryToChar){
+            foreach(KeyValuePair<string, char> kvp in binaryToChar){
                 Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
             }
         }
         else if(textToBinary == 'n'){
-            foreach(KeyValuePair<string, string> kvp in binaryToChar){
+            foreach(KeyValuePair<string, char> kvp in binaryToChar){
                 Console.WriteLine("Key = {0}, Value = {1}", kvp.Value, kvp.Key);
             }
         }
@@ -86,6 +86,22 @@ class Decode
 }
 class Encode
 {
+    public static string[] supportedEncoders =
+    {
+        "BINARY"
+    };
+    private static string[] unsupportedEncoders =
+    {
+        "MORSE",
+        // LOW PRIORITY AS OF NOW, MAINLY BECAUSE I NEED TO LEARN THE CIPHERS AND HOW TO ENCODE AND DECODE THEM.
+        "AMSCO","AUTOKEY","BACONIAN","BAZERIES","BEAUFORT","BIFID","CADENUS","CHECKERBOARD","COMPLETE COLUMNAR TRANSPOSITION","COMPRESSOCRAT",
+        "CONDI","CM BIFID","DIGRAFID","FOURSQUARE","FRACTIONATED MORSE","GRANDPRÉ","GRILLE","GROMARK","GRONSFELD","HEADLINES",
+        "HOMOPHONIC","INCOMPLETE COLUMNAR","INTERRUPTED KEY","KEY PHRASE","MONOME-DINOME","MORBIT","MYSZKOWSKI","NICODEMUS","NIHILIST SUBSTITUTION","NIHILIST TRANSPOSITION",
+        "NULL","NUMBERED KEY","PERIODIC GROMARK","PHILLIPS","PHILLIPS-RC","PLAYFAIR","POLLUX","PORTA","PORTAX","PROGRESSIVE KEY",
+        "QUAGMIRE I","QUAGMIRE II","QUAGMIRE III","QUAGMIRE IV","RAGBABY","RAILFENCE","REDEFENCE","ROUTE TRANSPOSITION","RUNNING KEY","SEQUENCE TRANSPOSITION",
+        "SERIATED PLAYFAIR","SLIDEFAIR","SWAGMAN","SYLLABARY","SYLLABARY FOREIGN CHARTS","TRIDIGITAL","TRIFID","TRI-SQUARE","TWIN BIFID","TWIN TRIFID",
+        "TWO-SQUARE","VARIANT","VIGENÈRE"  
+    };
     public static void Binary(string userInput)
     {
         string input = userInput;
@@ -106,12 +122,14 @@ class Ciphers
         static void Main(string[] args)
         {
             string cipherDirectionCheck;
-            Console.WriteLine("Encode or Decode");
+            Console.WriteLine("Encode or Decode (or shorthand)");
             cipherDirectionCheck = Console.ReadLine();
-            while (cipherDirectionCheck.ToLower() != "decode" && cipherDirectionCheck.ToLower() != "encode")
+            while (cipherDirectionCheck.ToLower() != "decode" && cipherDirectionCheck.ToLower() != "encode" && cipherDirectionCheck.ToLower()[0] != 's')
             {
                 cipherDirectionCheck = Console.ReadLine();
             }
+
+            //DECODE CHECKS
             if(cipherDirectionCheck.ToLower() == "decode"){
                 Console.WriteLine("Decode what?");
                 string decodeType = Console.ReadLine().Trim();
@@ -127,8 +145,55 @@ class Ciphers
                     Console.WriteLine("Unsupported method. Please check back later for new updates.");
                 }
             }
+
+            //ENCODING CHECKS
             else if(cipherDirectionCheck.ToLower() == "encode"){
-                Encode.Binary(Console.ReadLine());
+                Console.WriteLine("Encode what?");
+                string encodeType = Console.ReadLine().Trim();
+                if(Encode.supportedEncoders.Contains(encodeType.ToUpper())){
+                switch(encodeType.ToUpper()){
+                        case "BINARY":
+                            Encode.Binary(Console.ReadLine());
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Unsupported method. Please check back later for new updates.");
+                }
+            }
+
+            //SHORTHAND CODE CHECKS
+            else if (cipherDirectionCheck.ToLower()[0] == 's')
+            {
+                switch(cipherDirectionCheck.ToLower()[1])
+                {
+                    case 'd':
+                        switch (cipherDirectionCheck.ToLower()[2])
+                        {
+                            case 'b':
+                                Decode.Binary(Console.ReadLine());
+                                break;
+                            default:
+                                Console.WriteLine("Unsupported shorthand.");
+                                break;
+                        }
+                        break;
+                    case 'e':
+                        switch (cipherDirectionCheck.ToLower()[2])
+                        {
+                            case 'b':
+                                Encode.Binary(Console.ReadLine());
+                                break;
+                            default:
+                                Console.WriteLine("Unsupported shorthand.");
+                                break;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Unsupported shorthand.");
+                        break;
+                }
             }
             else
             {
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # AlternateRealityGames
 Tools to solve ARGs
+Mainly basing everything off [this](https://wiki.gamedetectives.net/w/ARG_Toolbox#Audio_Analysis). Let me know if anything useful might be missing.
+
+[Tools written in C#](/C#%20Tools/)
